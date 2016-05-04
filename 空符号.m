d2692df9f98ecf0e6635987bbea3d588279c1255
Sidney <zhongxilin@gmail.com>
2016-05-04 09:49:45 +0800

    Imported user code snippets

---
+++ 空符号.m
@@ -0,0 +1,10 @@
+// 空符号
+// 可以用来控制字符来跳转
+//
+// IDECodeSnippetCompletionPrefix: st
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 0E7D50D4-C484-4DDA-A837-9F9D6E0206FA
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+<#st#>
